2532fefe2cee372d5372e4289b58e3007d018627
jugabriel77 <68034390+jugabriel77@users.noreply.github.com>
2020-07-17 15:55:42 -0500

    Add files via upload

---
+++ CajaDeCompensacion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CajaDeCompensación
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Para conocer cual tarifa es aplicble a usted");
+            Console.Write("Ingrese el valor de su salario mensual: ");
+            int sal = int.Parse(Console.ReadLine());
+
+            double smmlv = sal / 877803;
+
+            if (smmlv < 2)
+            {
+                Console.WriteLine("Su tarifa correspondiente es la Tarifa A");
+            }
+            else if (2 <= smmlv && smmlv < 4)
+            {
+                Console.WriteLine("Su tarifa correspondiente es la Tarifa B");
+            }
+            else
+            {
+                Console.WriteLine("Su tarifa correspondiente es la Tarifa C");
+            }
+        }
+    }
+}
---
+++ CalculoCuotaModerada.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CalculoCuotaModeradora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            Console.WriteLine("Para conocer su tarifa y su cuota moderada:");
+            Console.WriteLine("Ingrese el valor de su salario (mensual): ");
+            int sal = int.Parse(Console.ReadLine());
+
+            double smmlv = sal / 877803;
+
+            if (smmlv < 2)
+            {
+                Console.WriteLine("Su tarifa es de $3.400COP correpondiente a la Cuota A");
+            }
+            else if (2 <= smmlv && smmlv < 5)
+            {
+                Console.WriteLine("Su tarifa es de $13.500COP correpondiente a la Cuota B");
+            }
+            else
+            {
+                Console.WriteLine("Su tarifa es de $35.600COP correpondiente a la Cuota C");
+            }
+        }
+    }
+}
