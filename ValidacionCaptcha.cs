a58cc496461fecb1eb642ad1e74271cc92f023c7
jugabriel77 <68034390+jugabriel77@users.noreply.github.com>
2020-08-13 14:24:29 -0500

    Add files via upload

---
+++ ValidacionCaptcha.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ValidacionCaptcha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, c=0, ronda=0, ans;
+            Random aleatorio = new Random();
+
+            while (ronda <= 3)
+            {
+                if (ronda == 3)
+
+                {
+                    Console.WriteLine("eres una maquina, fuera"); break;
+                }
+                a = aleatorio.Next(1, 11);
+                b = aleatorio.Next(1, 11);
+                c = a + b;
+                Console.WriteLine("Calcule el valor para c dado por: a+b=c");
+                Console.WriteLine("Siendo a:" + a);
+                Console.WriteLine("siendo b:" + b);
+                Console.WriteLine("El valor de c es:");
+                ans = int.Parse(Console.ReadLine());
+
+                if (ans == c)
+                {
+                    Console.WriteLine("Puede continuar.");
+                }
+                else
+                {
+                    Console.WriteLine("Validacion incorrecta, no puede continuar.");
+                    ronda += 1;
+                }
+            }
+        }
+    }
+}
